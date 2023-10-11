38726ebbda9f7ea76ac86bffc7c258b5e6e606ca
Barnabé Frimaudeau <frimaudeau.barnabe@gmail.com>
2023-10-11 18:10:47 -0400

    added the webdev.py file and defined the functions we will use

---
+++ searchdata.py
@@ -0,0 +1,17 @@
+def get_outgoing_links(URL):
+    return
+
+def get_incoming_links(URL):
+    return
+
+def get_page_rank(URL):
+    return
+
+def get_idf(word):
+    return
+
+def get_tf(URL, word):
+    return
+
+def get_tf_idf(URL, word):
+    return
