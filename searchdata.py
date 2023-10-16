e40d40f38c4c341ad915a1c50c72b69acf9635d2
Barnabé Frimaudeau <frimaudeau.barnabe@gmail.com>
2023-10-16 19:46:26 -0400

    Error in the searchdata.py file, will fix later

--- searchdata.py
+++ searchdata.py
@@ -1,17 +1,34 @@
+import webdev
+
+# If the given URL was not found during the crawling process, this function must return the value None.
 def get_outgoing_links(URL):
-    return
+    links = []
+    page_content = webdev.read_url(URL)
+
+    link_start = page_content.find("<a href=")
+    while link_start != -1:
+        opening_quote = page_content.find('"', link_start)
+        ending_quote = page_content.find('"', opening_quote + 1)
+        url = page_content[opening_quote, 1:ending_quote]
+        links.append[url]
+
+        link_start = page_content.find("<a href=")
+    
+    return links
+
+print(get_outgoing_links("http://people.scs.carleton.ca/~davidmckenney/tinyfruits/N-0.html"))
 
 def get_incoming_links(URL):
-    return
+    pass
 
 def get_page_rank(URL):
-    return
+    pass
 
 def get_idf(word):
-    return
+    pass
 
 def get_tf(URL, word):
-    return
+    pass
 
 def get_tf_idf(URL, word):
-    return
+    pass
