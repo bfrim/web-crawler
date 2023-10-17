77e4a15215fcca1e7c4d9a850422a2d1ee2e5018
KassemT76 <kassemtaha06@gmail.com>
2023-10-16 21:04:52 -0400

    good crawler done

--- crawler.py
+++ crawler.py
@@ -3,23 +3,50 @@ import webdev
 
 
 def crawl(seed):
-    webString = webdev.read_url(seed)
+    x = webdev.read_url(seed)
     urls = []
-    webString=webString.split("\n")
     
-    for i in webString:
-        index = i.find("href=")
-        lastindex = 0
-
-        if index != -1:
-            index+=6
-            for j in range(index+1, len(i)):
-                print(i[j])
-                if i[j] == "\"" or i[j] == "\'":
-                    urls.append(i[index:j])
-                    break
-            
-            
+    i=0
+    
+    print(x)
+    
+    while i < (len(x)):
+        if x[i] == "<":
+            #Detect title
+            if x[i+1] == "t":
+                i+=7
+                title = ""
+                while not(x[i]=="<" and x[i+1]=="/"):
+                    title += x[i]
+                    i+=1
+                print("Title:",title)
+                    
+            #Detect Paragraph
+            elif x[i+1] == "p":
+                i+=3
+                word = ""
+                while not(x[i]=="<" and x[i+1]=="/"):
+                    
+                    if word != "" and (x[i] == "" or x[i] == "\n"):
+                        # print(word)
+                        word = ""
+                    else:
+                        word += x[i]
+                    i+=1
+            #Detect Anchor
+            elif x[i+1] == "a":
+                i+=3
+                link = ""
+                while not(x[i]=="<" and x[i+1]=="/"):
+                    print(x[i-6:i])
+                    if x[i-6:i] == "href=\"":
+                        while x[i] != "\"" and x[i] != "\'":
+                            link+=x[i]
+                            i+=1
+                        urls.append(link)
+                    i+=1
+        
+        i+=1
     
     print(urls)
                 
@@ -30,3 +57,17 @@ def crawl(seed):
 p = crawl('http://people.scs.carleton.ca/~davidmckenney/tinyfruits/N-0.html')
 
 # print(p)
+
+
+    # for i in webString:
+    #     index = i.find("href=")
+    #     lastindex = 0
+
+    #     if index != -1:
+    #         index+=6
+    #         for j in range(index+1, len(i)):
+    #             print(i[j])
+    #             if i[j] == "\"" or i[j] == "\'":
+    #                 urls.append(i[index:j])
+    #                 break
+            
---
+++ test.py
@@ -0,0 +1,6 @@
+for i in range(10):
+    print(i)
+    if i % 2:
+        i += 2
+        print("help",i)
+    
